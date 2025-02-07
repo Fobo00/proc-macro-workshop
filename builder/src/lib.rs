cfc15b270ec34320d92776e6b92d491767cb0595
Fobo00 <73784545+Fobo00@users.noreply.github.com>
2025-02-07 15:34:18 +0000

    added 2 tests, in the middle of the second one

--- builder/src/lib.rs
+++ builder/src/lib.rs
@@ -2,7 +2,22 @@ use proc_macro::TokenStream;
 
 #[proc_macro_derive(Builder)]
 pub fn derive(input: TokenStream) -> TokenStream {
-    let _ = input;
+    let input = syn::parse_macro_input!(input as syn::DeriveInput);
 
-    unimplemented!()
+    let name = input.ident;
+    let builder_name = syn::Ident::new(&format!("{name}Builder"), name.span());
+
+    quote::quote! {
+        impl #name {
+            pub fn builder() -> #builder_name {
+                #builder_name {
+                    
+                }
+            }
+        }
+
+        struct #builder_name {
+
+        }
+    }.into()
 }
